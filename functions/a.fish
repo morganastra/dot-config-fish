fa839a1823b1ffd339906232ba440d5ae3d79937
Morgan Mullaney <jump@fastmail.fm>
2014-07-03 14:24:39 -0700

    return don't exit

--- functions/a.fish
+++ functions/a.fish
@@ -1,10 +1,10 @@
 function a
-    if hash ag 2>/dev/null
+    if which ag >/dev/null
         ag $argv;
-    else if hash ack 2>/dev/null
+    else if which ack >/dev/null
         ack $argv;
     else
         echo "Install ag or ack" 1>&2
-        exit 1
+        return 1
     end
 end
