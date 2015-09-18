da0ddc55af0db038c711a932d6b01d68bc429c77
Morgan Mullaney <jump@fastmail.fm>
2015-09-18 15:44:22 -0700

    dear fish, please stop adding hard tabs. love, morgan

--- functions/m.fish
+++ functions/m.fish
@@ -1,4 +1,4 @@
 function m
-	set m_files (find $argv -type f | egrep -i '.*\.(flac|alac|aac|ogg|mp3|m4a)$' | sort )
+    set m_files (find $argv -type f | egrep -i '.*\.(flac|alac|aac|ogg|mp3|m4a)$' | sort )
     mplayer $m_files
 end
