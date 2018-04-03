e614da4f62875cabc0a2081fadbe8bc18c6be26e
Morgan Astra <m@morganastra.me>
2018-04-03 13:51:53 -0700

    add support for ripgrep in search alias (`a`)

--- functions/a.fish
+++ functions/a.fish
@@ -1,10 +1,16 @@
+# Defined in - @ line 2
 function a
-    if which ag >/dev/null
-        ag $argv;
+	if which rg >/dev/null
+        rg $argv
+
+    else if which ag >/dev/null
+        ag $argv
+
     else if which ack >/dev/null
-        ack $argv;
+        ack $argv
+
     else
-        echo "Install ag or ack" 1>&2
+        echo "Install rg or ag or ack" 1>&2
         return 1
     end
 end
