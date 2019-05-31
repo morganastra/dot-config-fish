a494d6d1eb75e96dba583a8ab28e155515d8bd50
morgan <morgan@morgans-MacBook-Pro.local>
2019-05-31 11:49:24 -0700

    Add direnv and restructure aliases

--- config.fish
+++ config.fish
@@ -2,7 +2,12 @@ set PATH $HOME/.cargo/bin $HOME/.local/bin $PATH
 
 set -x N_PREFIX $HOME/.local
 
-abbr -a g git
-abbr qr 'cat | qrencode -tANSIUTF8 -o-'
+eval (direnv hook fish)
+
+if status --is-interactive
+	abbr --add --global g git
+	abbr --add --global da 'direnv add'
+	abbr --add --global qr 'cat | qrencode -tANSIUTF8 -o-'
+end
 
 thefuck --alias | source
