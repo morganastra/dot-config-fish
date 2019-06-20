f2cdc999f15b966bd53926ef4d655f2197265a21
Morgan Astra <m@morganastra.me>
2019-06-20 11:13:17 -0700

    Put interactive stuff in --is-interactive

--- config.fish
+++ config.fish
@@ -2,24 +2,26 @@ set PATH $HOME/.cargo/bin $HOME/.local/bin $PATH
 
 set -x N_PREFIX $HOME/.local
 
-fish_vi_key_bindings
-
-if which direnv > /dev/null 2>&1
-	eval (direnv hook fish)
-	echo "Setting direnv hook"
-else
-	echo "direnv not installed"
-end
-
-if which thefuck > /dev/null 2>&1
-	thefuck --alias | source
-else
-	echo "thefuck not installed"
-end
-
 if status --is-interactive
+
+    fish_vi_key_bindings
+
+    if which direnv > /dev/null 2>&1
+	      eval (direnv hook fish)
+	      echo "Setting direnv hook"
+    else
+	      echo "direnv not installed"
+    end
+
+    if which thefuck > /dev/null 2>&1
+	      thefuck --alias | source
+    else
+	      echo "thefuck not installed"
+    end
+
 	abbr --add --global g git
 	abbr --add --global da 'direnv allow'
 	abbr --add --global qr 'cat | qrencode -tANSIUTF8 -o-'
+  abbr --add --global cljr 'clj -A"dev" -e "(dev)(go)" -r'
 end
 
