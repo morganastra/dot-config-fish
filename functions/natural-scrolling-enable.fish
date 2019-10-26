67d984fb1862663031d30969c2dc62d49f94f243
Morgan Astra <m@morganastra.me>
2019-10-25 17:08:47 -0700

    Some improvements

--- functions/natural-scrolling-enable.fish
+++ functions/natural-scrolling-enable.fish
@@ -1,5 +1,5 @@
-# Defined in /tmp/fish.lXG6vC/natural-scrolling-enable.fish @ line 2
+# Defined in - @ line 2
 function natural-scrolling-enable
-	xinput list | grep -v Virtual | grep "id=" | grep pointer | cut -f2 | cut -d= -f2 | xargs -I% xinput set-prop % 289 1 | true
-	xinput list | grep -v Virtual | grep "id=" | grep pointer | cut -f2 | cut -d= -f2 | xargs xinput list-props | grep 289
+	xinput list | grep -v Virtual | grep "id=" | grep pointer | cut -f2 | cut -d= -f2 | xargs -I% xinput set-prop % "libinput Natural Scrolling Enabled" 1 | true
+    xinput list | grep -v Virtual | grep "id=" | grep pointer | cut -f2 | cut -d= -f2 | xargs xinput list-props | grep "libinput Natural Scrolling Enabled"
 end
