f06012a5093562d30a6b52534038206aac1a5669
Morgan Astra <m@morganastra.me>
2021-02-16 14:47:32 -0800

    Handle lack of search term gracefully

--- functions/aurs.fish
+++ functions/aurs.fish
@@ -1,6 +1,8 @@
 # Defined in - @ line 2
 function aurs
-    pacman -Ss $argv[1]
-
-  	curl -Ls "https://aur.archlinux.org/rpc/?v=5&type=search&arg="$argv[1] | jq -r '.results[] | (.Name + "|" + .Description)' | column -t -s '|'
+    if count $argv > /dev/null
+      curl -Ls "https://aur.archlinux.org/rpc/?v=5&type=search&arg="$argv[1] | jq -r '.results[] | (.Name + "|" + .Description)' | column -t -s '|'
+  else
+      echo "Please supply a search term"
+    end
 end
