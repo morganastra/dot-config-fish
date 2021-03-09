6b38f9f4f73ecce7d2000de38dbb2133a387b38b
Morgan Astra <m@morganastra.me>
2021-03-08 20:00:42 -0800

    remove some unused config

--- config.fish
+++ config.fish
@@ -6,19 +6,6 @@ if status --is-interactive
 
     fish_vi_key_bindings
 
-    if which direnv > /dev/null 2>&1
-	      eval (direnv hook fish)
-	      echo "Setting direnv hook"
-    else
-	      echo "direnv not installed"
-    end
-
-    if which thefuck > /dev/null 2>&1
-	      thefuck --alias | source
-    else
-	      echo "thefuck not installed"
-    end
-
 	abbr --add --global g git
 	abbr --add --global da 'direnv allow'
 	abbr --add --global qr 'cat | qrencode -tANSIUTF8 -o-'
