e2ecf9c5b0d06b727297e2b4dd23a717d584373c
Morgan Astra <morganastra@proton.me>
2026-02-11 18:43:07 -0800

    Handle edge case in kernel version checking

--- functions/check_kernel_update.fish
+++ functions/check_kernel_update.fish
@@ -1,6 +1,6 @@
 function check_kernel_update
-    set running_kernel (uname -r)
-    set installed_kernel (pacman -Q linux | cut -d' ' -f2)
+    set running_kernel (uname -r | sed 's/-/./g')
+    set installed_kernel (pacman -Q linux | cut -d' ' -f2 | sed 's/-/./g') 
 
     if test "$running_kernel" != "$installed_kernel"
         echo "new kernel installed - please reboot!"
