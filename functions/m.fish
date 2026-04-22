613ebb61c375fbdb474a84e9aa4f6d590756e5be
Morgan Astra <morganastra@proton.me>
2026-04-22 11:40:23 -0700

    Simplify music arg handling

--- functions/m.fish
+++ functions/m.fish
@@ -1,7 +1,15 @@
 function m
-	if pgrep -x audacious >/dev/null
-		audacious -e (findmusic $argv | sort)
-	else
-		mpv --no-video (findmusic $argv | sort)
-	end
+ # Play music from a given directory using an appropriate player
+
+ set -l _flag_e # enqueue (only supported in audacious)
+ set -l _flag_R # play in shuffled order
+
+ argparse 'R/random-sort' 'e/enqueue' -- $argv
+ or return
+
+ if pgrep -x audacious >/dev/null
+     audacious $_flag_e (findmusic $argv | sort $_flag_R)
+ else
+   mpv --no-video (findmusic $argv | sort $_flag_R)
+ end
 end
--- functions/mshuf.fish
+++ functions/mshuf.fish
@@ -1,7 +1,3 @@
 function mshuf
-	if pgrep -x audacious >/dev/null
-		audacious -e (findmusic $argv | sort -R)
-	else
-		mpv --no-video (findmusic $argv | sort -R)
-	end
+    m -R $argv
 end
