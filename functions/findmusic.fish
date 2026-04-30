b8e3066006be7dc77b41dab6637824f0e602672f
Morgan Astra <morganastra@proton.me>
2026-04-29 22:02:53 -0700

    Handle additional file types for music functions

--- functions/findmusic.fish
+++ functions/findmusic.fish
@@ -1,3 +1,7 @@
 function findmusic
-	find $argv -type f | grep -E -i '.*\.(flac|aiff|mp4|webm|mkv|alac|aac|ogg|mp3|m4a)$'
+	# Edge case: when trying to find music across nested directories
+	# where some contain .cue files and others do not, only .cue files
+	# will be returned.
+	# find $argv -type f | grep -E -i '.*\.cue$'; or \
+	find $argv -type f | grep -E -i '.*\.(cue|ape|flac|aiff|mp4|webm|mkv|alac|aac|ogg|mp3|m4a)$'
 end
